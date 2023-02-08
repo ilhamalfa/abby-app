3eeb810f190d889d04a0223172c2923081d8571d
yanfanurfarabi <70649118+yanfanurfarabi@users.noreply.github.com>
2023-02-08 10:55:33 +0700

    Add files via upload

---
+++ resources/views/gallery/category/autodetailing.blade.php
@@ -0,0 +1,37 @@
+@extends('gallery.gallery')
+
+@section('content')
+
+<section>
+  <div class="gallery-image">
+    <div class="gallery-canvas">
+    
+    <a href="" class="gallery-item mx-auto">
+      <img src="img/12.jpg" class="rounded mx-auto d-block gal-img" alt="...">
+    </a>
+    <a href="" class="gallery-item mx-auto">
+    <img src="img/13.jpg" class="rounded mx-auto d-block gal-img" alt="...">
+  </a>
+  <a href="" class="gallery-item mx-auto">
+    <img src="img/14.jpg" class="rounded mx-auto d-block gal-img" alt="...">
+  </a>
+  <a href="" class="gallery-item mx-auto">
+    <img src="img/12.jpg" class="rounded mx-auto d-block gal-img" alt="...">
+  </a>
+  <a href="" class="gallery-item mx-auto">
+    <img src="img/13.jpg" class="rounded mx-auto d-block gal-img" alt="...">
+  </a>
+    <a href="" class="gallery-item mx-auto">
+    <img src="img/14.jpg" class="rounded mx-auto d-block gal-img" alt="...">
+    </a>
+    <a href="" class="gallery-item mx-auto">
+    <img src="img/12.jpg" class="rounded mx-auto d-block gal-img" alt="...">
+    </a>
+    <a href="" class="gallery-item mx-auto">
+    <img src="img/13.jpg" class="rounded mx-auto d-block gal-img" alt="...">
+    </a>
+  </div>
+</div>
+</section>
+
+@endsection
