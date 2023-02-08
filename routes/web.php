b55aa2f0df6313f484a0853e054aaa122484cc3f
yanfanurfarabi <70649118+yanfanurfarabi@users.noreply.github.com>
2023-02-08 11:01:36 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,33 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/caraudio', function () {
+    return view('audio.audio');
+});
+
+Route::get('/autodetailing', function () {
+    return view('auto.auto');
+});
+
+Route::get('/gallery', function () {
+    return view('gallery.category.showroom');
+});
+
+Route::get('/gallery-showroom', function () {
+    return view('gallery.category.showroom');
+});
+
+Route::get('/gallery-caraudio', function () {
+    return view('gallery.category.caraudio');
+});
+
+Route::get('/gallery-autodetailing', function () {
+    return view('gallery.category.autodetailing');
+});
+
+Route::get('/profile', function () {
+    return view('profile.profile');
 });
