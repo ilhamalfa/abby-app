143c35e900e521541490cca423728d11955ede79
yanfanurfarabi <70649118+yanfanurfarabi@users.noreply.github.com>
2023-02-08 10:56:21 +0700

    Create profile.blade.php

---
+++ resources/views/profile/profile.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.index')
+
+@section('content')
+
+<div id="profilebanner" class="carousel slide">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="img/14.jpg" class="d-block w-100" alt="#bannerprofile">
+      </div>
+    </div>
+
+@endsection
