fe6d11c612e95834382b0b3c3d2ee133255df37c
yanfanurfarabi <70649118+yanfanurfarabi@users.noreply.github.com>
2023-02-23 11:57:46 +0700

    Update profile.blade.php

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -8,5 +8,28 @@
         <img src="img/14.jpg" class="d-block w-100" alt="#bannerprofile">
       </div>
     </div>
+    
+    <section id="profile-page">
+  
+      
+      <div class="canvas-profile">
+
+        <div class="title-frame">
+          <span class="title" id="profile">PREMIO</span>
+        </div>
+
+        <div class="body-frame" id="body-profile">
+          <span>premio adalah workshop yang terdiri dari 2 lini yaitu car audio dan auto detailing
+          </span>
+        </div>
+
+        <div class="canvas-image" id="img-profile">
+          <img src="img/13.jpg" class="img-fluid" alt="#lounge">
+          </div>
+
+      </div>
+      
+      <div class="separator"></div>
+    </section>
 
 @endsection
